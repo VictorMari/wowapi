c5a425481700c94516ad2cd058de05f6f88a2194
Victor Mari Rodriguez <vrodriguez@tmforum.org>
2022-08-06 22:01:29 +0200

    Adding mongodb store

---
+++ atlas-store.py
@@ -0,0 +1,45 @@
+import json
+import pymongo
+import sys
+import os
+from pathlib import Path
+
+class AtlasStore:
+    def __init__(self) -> None:
+        self.client = pymongo.MongoClient(f"mongodb+srv://{os.getenv('MONGO_USER')}:{os.getenv('MONGO_PWD')}@cluster0.k0kxo.mongodb.net/?retryWrites=true&w=majority")
+        self.db = self.client['auctions']
+
+    def insert_report(self, report: dict) -> None:
+        realm = report['realm_id']
+        collection = self.db[realm]
+        for item_id, auctions in report['auctions'].items():
+            collection.find_one_and_update(
+                filter={'_id': item_id}, 
+                upsert=True,
+                update={'$addToSet': {'auctions': auctions}}               
+            )
+            
+         
+                
+
+
+def load_auction_reports():
+    for path in Path("AuctionsProcessed").glob("*.json"):
+        print(f"loading {path}")
+        with open(path) as f:
+            yield json.load(f)
+
+
+
+
+
+def main():
+    atlas_store = AtlasStore()
+    for report in load_auction_reports():
+        atlas_store.insert_report(report)
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ auctionAggregator.py
@@ -0,0 +1,39 @@
+import json
+import sys
+from pathlib import Path
+
+
+def load_auction_reports():
+    for path in Path("auctioncached").glob("*.json"):
+        print(f"loading {path}")
+        with open(path) as f:
+            yield json.load(f)
+    
+
+def aggregateAuctions(report):
+    auction_table = {}
+    for auction in report["auctions"]:
+        item_id = auction["item"]["id"]
+        if item_id not in auction_table:
+            auction_table[item_id] = []
+        del auction["item"]["id"]
+        if len(auction["item"].keys()) == 0:
+            del auction["item"]
+        auction_table[item_id].append(auction)
+
+    return auction_table
+
+
+
+def main():
+    Path("AuctionsProcessed").mkdir(parents=True, exist_ok=True)
+    for report in load_auction_reports():
+        ah_table = aggregateAuctions(report)
+        report["auctions"] = ah_table
+        with Path(f"AuctionsProcessed/{report['realm_id']}.json").open("w+") as f:
+            json.dump(report, f, indent=4)
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
